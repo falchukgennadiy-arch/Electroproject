d41ab81b7e76cdf9a1ebd4cdb432ad85cf4411e0
falchukgennadiy-arch <falchukgennadiy@gmail.com>
2026-03-24 21:37:36 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,16 @@
+// ===== КОНФИГУРАЦИЯ ПРИЛОЖЕНИЯ =====
 const CONFIG = {
   API_URL: 'https://api.omavisual.ru/api',
   SITE_NAME: 'ЭлектроПроект Donuts',
-  VK_APP_ID: '54458443'
+  VK_APP_ID: '54458443',
+  
+  // Ссылки на подписки VK Donut
+  DONUT_LINKS: {
+    course: 'https://vk.com/electrocourses?w=donut_payment-223389702&levelId=2499',
+    visual: 'https://vk.com/electrocourses?w=donut_payment-223389702&levelId=2500',
+    template: 'https://vk.com/electrocourses?w=donut_payment-223389702&levelId=2501',
+    test: 'https://vk.com/electrocourses?w=donut_payment-223389702&levelId=2502'
+  }
 };
+
+window.CONFIG = CONFIG;
